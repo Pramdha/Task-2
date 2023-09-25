5efc0af2210b6b1805c5d6ab0588497cc2c40726
Pramdha <145755265+Pramdha@users.noreply.github.com>
2023-09-25 21:56:54 +0530

    Add files via upload

---
+++ Task 2.py
@@ -0,0 +1,20 @@
+
+
+List1 = [12,-7,5,64,-14]
+num = 0
+
+while ( num < len(List1)):
+ if List1[num] >= 0:
+  print( List1[num])
+ num=num+1
+
+
+ List2 = [12,14,-95,3]
+num = 0
+
+while ( num < len(List2)):
+ if List2[num] >= 0:
+  print( List2[num])
+ num=num+1
+ 
+    
